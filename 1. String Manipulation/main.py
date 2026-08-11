0880ba9e66127ac529febb2b13fcd0b31372f438
sumitmukharjeeee <sumitmukharjee619@gmail.com>
2026-08-11 20:14:16 +0530

    string manipulation topic begins

--- 1. String Manipulation/main.py
+++ 1. String Manipulation/main.py
@@ -2,3 +2,9 @@
 
 name = "Michael Jackson"
 print(name + "is the best")
+print(3* "MJ ") 
+name = name +"is the best"
+print(name)
+print("Michael Jackson is \t is best")
+print("Michael Jackson is \n is best")
+print("Michael Jackson is \\t is best")
