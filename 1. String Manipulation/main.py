a25b45475c1a62096b329e674c39fe291cfd87fd
sumitmukharjeeee <sumitmukharjee619@gmail.com>
2026-08-12 09:43:42 +0530

    string manipulation

--- 1. String Manipulation/main.py
+++ 1. String Manipulation/main.py
@@ -8,3 +8,26 @@ print(name)
 print("Michael Jackson is \t is best")
 print("Michael Jackson is \n is best")
 print("Michael Jackson is \\t is best")
+
+print(r"Michael Jackson is \t is best") # r means raw string 
+
+# String Methods
+
+a = "Thriller is the sixth genre"
+print(a.upper())
+print(a.replace("Thriller","Comedy"))
+print(a)
+
+print(a.find("sixthsdf"))
+
+# format strings
+
+name = "Dua Lipa"
+age = 35
+print(f"My name is {name} and I am {age} years old")
+print("My name is {} and I am {} years old".format(name, age))
+
+name  = "Jonathan"
+age = 40
+
+print("Ny name is %s and I am %d years old" %(name, age))
