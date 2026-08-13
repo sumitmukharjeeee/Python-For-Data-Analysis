0cdf46f5209be56464c02e07e13f5ce1b1f26407
sumitmukharjeeee <sumitmukharjee619@gmail.com>
2026-08-14 02:12:10 +0530

    Loops started

---
+++ 3. Conditionals and Branching/loops.py
@@ -0,0 +1,3 @@
+primeNumbers = [2,3,5,7]
+for prime in primeNumbers:
+    print(prime)
--- 3. Conditionals and Branching/main.py
+++ 3. Conditionals and Branching/main.py
@@ -1,1 +1,24 @@
 print(6==7)
+print("ACDC" == "MJ")
+
+if 6 == 7:
+    print("Yes")
+else:
+    print("No")
+
+age = 17
+if age > 18:
+    print("You can enter")
+print("Move on")
+
+age2 = 17
+if age2 > 18:
+    print("You can enter")
+elif age2 == 18:
+    print("You can enter")
+else:
+    print("get off")
+print("Move on")
+print('a' == 'A')
+# print(1=2) error cant assign literal
+
