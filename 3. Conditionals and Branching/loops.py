5db374855c0da33b32d0fd2797814df6f9ee39f0
sumitmukharjeeee <sumitmukharjee619@gmail.com>
2026-08-15 13:24:04 +0530

    Functions done

--- 3. Conditionals and Branching/loops.py
+++ 3. Conditionals and Branching/loops.py
@@ -1,3 +1,34 @@
 primeNumbers = [2,3,5,7]
 for prime in primeNumbers:
     print(prime)
+
+for i in range(1,5):
+    print(i*2)
+
+squares = ["red","yellow","green","purple","blue"]
+# for i in range(0,5):
+#     squares[i] = "white"
+# print(squares)
+
+for square in squares:
+    print(square)
+
+squares2 = ["red","yellow","green","purple","blue"]
+for i,  color in enumerate(squares):
+    print(f"{i}: {color}")
+# This gives values with position
+
+for number in range(11):
+    print(number)
+
+superheroes = ["Doom","F4","Superman"]
+for i, hero in enumerate(superheroes):
+    print(f"{i}: {hero}")
+
+count = 1
+while count<=20:
+    print(count)
+    count +=1
+
+for x in ['A', 'B', 'C']:
+    print(x + 'A')
---
+++ 3. Functions/main.py
@@ -0,0 +1,36 @@
+def functiion(a):
+    b = a + 1
+    print(a, "+ 1 = ",b)
+    return b
+
+functiion(5)
+functiion(4)
+functiion(3)
+functiion(2)
+functiion(1)
+
+def total(a,b):
+    totals = a+b
+    return totals
+
+result = total(5,6)
+print(result)
+
+str_length = len("Hello World")
+list_lenth = len([1,2,3,4,5])
+
+print(str_length)
+print(list_lenth)
+
+total = sum([10,20,40])
+print(total)
+
+print(max([10,20,30]))
+print(min([10,20,30]))
+
+def greet(name):
+    return "Hello " + name
+
+print(greet("Sumit")) 
+print(greet("Tanu")) 
+print(greet("Abs")) 
